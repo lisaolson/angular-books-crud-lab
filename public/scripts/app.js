22e9c9fca1bf5340f8d3f9ccb72e8e3eabb78fed
Lisa Olson <olson.lisa94@gmail.com>
2016-09-17 15:53:59 -0700

    Adds BookController function &  GET request

---
+++ public/scripts/app.js
@@ -0,0 +1,24 @@
+console.log('app.js is linked!');
+
+angular
+  .module('books', [])
+  .controller('BooksIndexController', BooksIndexController);
+
+  BooksIndexController.$inject=['$http'];
+  function BooksIndexController($http) {
+    var vm = this;
+
+      $http({
+        method: 'GET',
+        url: "https://super-crud.herokuapp.com/books"
+      }).then(onBooksIndexSuccess, onError)
+
+      function onBooksIndexSuccess(response){
+        console.log('get request for books', response.data);
+        vm.books = response.data.books;
+      }
+
+      function onError(error){
+        console.log('there was an error: ', error);
+      }
+  };
