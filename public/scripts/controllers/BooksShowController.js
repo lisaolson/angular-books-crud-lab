7478d3ad87c247f618ca677b329ed67b2adee6b2
Lisa Olson <olson.lisa94@gmail.com>
2016-09-18 18:10:45 -0700

    Adds get method for specific book

---
+++ public/scripts/controllers/BooksShowController.js
@@ -0,0 +1,23 @@
+angular
+  .module('booksApp')
+  .controller('BooksShowController', BooksShowController);
+
+  BooksShowController.$inject=['$http', '$routeParams', $'location'];
+  function BooksShowController($http, $routeParams, $location) {
+    var vm=this;
+    var bookId = $routeParams.id;
+    $http({
+      method: 'GET',
+      url: 'https://super-crud.herokuapp.com/books/' +bookId
+    }).then(onBookShowSuccess, onError);
+
+    function onBookShowSuccess(response){
+      console.log('data for book ', bookId, ':', response.data);
+      vm.book = response.data;
+    }
+
+    function onError(error){
+      console.log('There was an error ', error);
+    }
+    
+  }
