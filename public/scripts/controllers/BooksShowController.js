f812af9cd065597e4e4335dfd1663d6e3311b324
Lisa Olson <olson.lisa94@gmail.com>
2016-09-18 19:51:04 -0700

    Sets up controller for Delete and Update functionality

--- public/scripts/controllers/BooksShowController.js
+++ public/scripts/controllers/BooksShowController.js
@@ -20,4 +20,36 @@ angular
       console.log('There was an error ', error);
     }
 
+    vm.updateBook = function(updatedBook) {
+      console.log('updated book ', updatedBook);
+      $http({
+        method: 'PUT',
+        url: 'https://super-crud.herokuapp.com/books' + updatedBook._id,
+        data: {
+          title: updatedBook.title,
+          author: updatedBook.author,
+          image: updatedBook.image,
+          releaseDate: updatedBook.releaseDate
+        }
+      }).then(onUpdateBookSuccess, onError);
+
+      function onUpdateBookSuccess(response){
+        console.log('Updated Book', bookId, ':', response.data);
+        vm.book = response.data;
+        $location.path('/');
+      }
+    };
+
+    vm.deleteBook = function(book) {
+      console.log('deleting book: ', book);
+      $http({
+        method: 'DELETE',
+        url: 'https://super-crud.herokuapp.com/books/'+book._id,
+      }).then(onDeleteBookSuccess, onError);
+
+      function onDeleteBookSuccess(response){
+        console.log('Deleted book', response.data);
+        $location.path('/');
+      }
+    };
   }
